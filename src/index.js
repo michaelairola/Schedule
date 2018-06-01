be1dd438e3093c21be3c581d5b7f8bfe332a7064
michaelairola <mikeairola@mac.com>
2018-06-01 13:06:10 -0700

    fixed next event box size issue

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App 
+ReactDOM.render(<App
   testCalendar='j9li06635odqicqeifcuhq4tko@group.calendar.google.com'
   publicTestCalendar='rkum1mpf9km93b6io7dgniramg@group.calendar.google.com'
-  chsVolleyballCalendar='tfd0nd7cddaj0l3k7k9qcrl81o@group.calendar.google.com'/>, 
+  chsVolleyballCalendar='tfd0nd7cddaj0l3k7k9qcrl81o@group.calendar.google.com'/>,
   document.getElementById('root'));
 registerServiceWorker();
